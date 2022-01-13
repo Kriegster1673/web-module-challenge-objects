9735a5a8466c48b65574edaf6794557b8d99ef03
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-01-13 13:13:20 -0600

    finished task 6, realized I can fully reference an array

--- index.js
+++ index.js
@@ -111,8 +111,8 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(array, number) {
+  return `${array[number].name} gave the restaurant a ${array[number].rating} star review, and their feedback was: ${array[number].feedback}`
 }
 
 
